266771c1c9ed9c89d312f9f3f27a678ac3527920
vortydev <etienne.dmenard@gmail.com>
2021-11-29 11:07:48 -0500

    page d'accueil

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php 
     if (session_status() === PHP_SESSION_NONE) session_start();
     require_once './inc/header.php';
-    echo 'index.php';
+?>
+
+<h1>Pour une configuration sans trop de réflexions.</h1>
+<h2>Configuration Suprême est un outil <em>simple</em> et <em>gratuit</em> vous permettant de configurer l'ordinateur de vos rêves, 
+    tout en validant la compatibilité des pièces choisies.</h2>
+<h2>Qu'attendez-vous? Commencez à configurer <em>maintenant</em>!</h2>
+
+<?php
     require_once './inc/footer.php';
 ?>
