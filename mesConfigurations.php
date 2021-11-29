a3fc311a10864fa8aef3176fa1350c11045f67de
vortydev <etienne.dmenard@gmail.com>
2021-11-29 09:19:27 -0500

    nommé le fichier correctement

--- mesConfigurations.php
+++ mesConfigurations.php
@@ -1,6 +1,6 @@
 <?php 
     if (session_status() === PHP_SESSION_NONE) session_start();
     require_once './inc/header.php';
-    echo 'mesConnexions.php';
+    echo 'mesConfigurations.php';
     require_once './inc/footer.php';
 ?>
