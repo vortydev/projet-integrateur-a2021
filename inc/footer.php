99e24c834b970b7a895c712c67072de03f4ed623
vortydev <etienne.dmenard@gmail.com>
2021-11-29 10:30:17 -0500

    CSS de base ready

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
     </main>
     <footer>
-        <p>2021 © Étienne Ménard, Felix Alberto Albarracin Oviedo, Pier-Olivier Corriveau, Zachary Blais</p>
+        <p>2021 © Étienne Ménard, Felix Alberto Albarracin Oviedo, Pier-Olivier Corriveau</p>
     </footer>
 </body>
 </html>
