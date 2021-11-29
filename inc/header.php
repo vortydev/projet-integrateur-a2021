ca4ffa7d8a1cc98591b07a35427ccab6804d9043
vortydev <etienne.dmenard@gmail.com>
2021-11-29 14:31:19 -0500

    mini typo

--- inc/header.php
+++ inc/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <title>Configurateur Suprême</title>
+    <title>Configuration Suprême</title>
 
     <link rel="stylesheet" href="./css/style.css" />
 
