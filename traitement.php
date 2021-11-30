d962dd236b47265c79c5f64810be6c93e6c62d2d
DrMaiis <pierrotc@hotmail.ca>
2021-11-30 18:55:07 -0500

    debut traitement inscription

--- traitement.php
+++ traitement.php
@@ -2,5 +2,16 @@
     if (session_status() === PHP_SESSION_NONE) session_start();
     require_once './inc/header.php';
     echo 'traitement.php';
+   
+    if (isset($_REQUEST['action'])){
+        if ($_REQUEST['action'] == "inscription"){
+            //section entrer de donne client dans base de donne
+            
+            
+            $new_Client = new Client ()
+        }
+    } 
+    else {}
+
     require_once './inc/footer.php';
 ?>
