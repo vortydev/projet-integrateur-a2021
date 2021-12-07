cd8292b9185a7dd750985c0c03a2248aeef90aa6
DrMaiis <pierrotc@hotmail.ca>
2021-12-06 19:21:02 -0500

    jcomprend rien

---
+++ inc/INSERTION PROJET.sql
@@ -0,0 +1,146 @@
+
+INSERT INTO Client(prenom, nom,courriel, motDePasse, dateNaissance, adresse)
+values  ("Alberto","Oviedo","alboviedo@hotmail.com","alboviedo","1995-02-11","168 Rue Mont-Plaisant"),
+("Etienne","Menard","etimenard@hotmail.com","etimenard","2001-01-11","199 Rue Sarah"),
+("Pier-Olivier","Corriveau","pocorriveau@hotmail.com","pocorriveau","2002-09-01","01 Bacon"),
+("Camilo","Torres","camtorres@hotmail.com","camtorres","1992-10-11","05 Des boises"),
+("Karen","Torres","kartorres@hotmail.com","kartorres","1995-12-28","16 Rue Darche"),
+("Samuel","Gagnon","samgagnon@hotmail.com","samgagnon","2005-10-12","34 vaisseau d'or"),
+("Shelane","Kathe","shekate@hotmail.com","shekate","1999-05-05","4 Rue Montreal"),
+("Francis","Perron","fraperron@hotmail.com","fraperron","1994-02-18","50 Rue Evageline"),
+("Jimmy","Peterson","jimpeterson@hotmail.com","jimpeterson","1984-11-06","19 La cantera"),
+("Charlotte","Thevenin","chathevenin@hotmail.com","chathevenin","1993-11-19","168 Rue Mont-Plaisant");
+
+
+
+
+INSERT INTO Fabricant(nom)
+Values ("Asus"),
+("MSI"),
+("ASRock"),
+("Gigabyte"),
+("Intel"),
+("Noctua"),
+("NZXT"),
+("Corsair"),
+("Silverstone"),
+("ZALMAN"),
+("Swiftech"),
+("T-Force"),
+("OWC"),
+("Richer-R"),
+("G.SKILL"),
+("Crucial"),
+("Seagate"),
+("Samsung"),
+("Western Digital"),
+("DELL"),
+("HP"),
+("NVIDIA"),
+("EVGA"),
+("Zotac"),
+("Liean Li"),
+("Phanteks"),
+("Cooler Master"),
+("BitFenix"),
+("Thermaltake"),
+("Fractal Desing");
+
+
+INSERT INTO Socket(nom)
+VALUES ("AM4"),
+("LGA1150"),
+("LGA1151"),
+("AM3+"),
+("FCBGA1528"),
+("SP3"),
+("sTR4"),
+("FCLGA1200"),
+("PGA988A"),
+("FCBGA1449"),
+("F(1207)"),
+("LGA2011-0"),
+("LGA2011-3"),
+("LGA1156"),
+("LGA1155"),
+("AM2"),
+("AM2+"),
+("AM3"),
+("FM1"),
+("FM2"),
+("FM2+"),
+("LGA1200"),
+("LGA1366"),
+("LGA2066"),
+("LGA775"),
+("LGA2011"),
+("LGA115X"),
+("LGA2011-V3");
+
+
+
+INSERT INTO SupportUSB(nom)
+VALUES ("USB 3.2 GEN 1, USB 3.2 GEN 2, USB 2.0"),
+("USB 3.2 GEN 1, USB 2.0"),
+("USB 3.2 Gen 2 Type-C, USB 3.2 Gen 1 Type-A"),
+("USB 3.2 Gen 1 Type-A, USB 2.0 Type-A"),
+("USB 3.2 Gen 1 Type-A"),
+("USB 3.2 Gen 1"),
+("USB 3.2 GEN 2"),
+("USB 2.0"),
+("USB 3.2 GEN 2 Type-A"),
+("USB 3.2 GEN 2 Type-C");
+
+
+
+INSERT INTO TypeMemoire(nom)
+VALUES ("DDR2"),
+("DDR3"),
+("DDR4"),
+("DDR5"),
+("DDR6"),
+("GDDR5X"),
+("GDDR6"),
+("GDDR6X"),
+("GDDR3"),
+("GDDR4");
+
+
+INSERT INTO TypeBoitier(nom)
+VALUES ("ATX Mid Tower"),
+("ATX Full Tower"),
+("Micro ATX Mini Tower"),
+("Mini ITX Tower"),
+("Mini ITX Test Bench"),
+("MicroATX Slim"),
+("HTPC"),
+("MicroATX Desktop"),
+("ATX Desktop"),
+("ATX Test Bench");
+
+
+INSERT INTO FormeCarteMere(nom)
+VALUES("ATX"),
+("Micro ATX"),
+("Mini ITX"),
+("EATX"),
+("HPTX"),
+("Thin Mini ITX"),
+("Mini DTX"),
+("SSI CEB"),
+("SSI EEB"),
+("XL ATX");
+
+INSERT INTO Connecteur(nom)
+VALUES ("PCI x16"),
+("DIMM"),
+("SO-DIMM"),
+("SATA"),
+("M.2"),
+("SAS2"),
+("NVME.2"),
+("PCI-EX16"),
+("PCI-E X1"),
+("PCI-EX16, PCI-E X1"),
+("M.2, SATA 6GB/s");
+
